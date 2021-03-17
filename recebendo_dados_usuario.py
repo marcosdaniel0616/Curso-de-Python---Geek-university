b0f18a57384805be323100a299739dea6c955c0a
Marcos Daniel <marcos.daniel0616@gmail.com>
2021-03-17 18:31:40 -0300

    Nova aula de recebimento de dados

---
+++ recebendo_dados_usuario.py
@@ -0,0 +1,11 @@
+"""
+Recebendo dados do usuário
+"""
+#Entrada de dados
+print('Qual seu nome?')
+nome = input() # Input -> Entrada
+
+#processamento
+
+#saída de dados
+print('Seja bem vindo(a): %s' %nome)
