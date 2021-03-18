ca020ed98cfd526954792b97fccaf59c0e527cba
Marcos Daniel <marcos.daniel0616@gmail.com>
2021-03-18 13:40:11 -0300

    Aula de tipo numérico finalizada!

---
+++ tipo_numerico.py
@@ -0,0 +1,25 @@
+"""
+Tipo numérico
+"""
+# você pode usar um _ (underline) para separar milhares em Python
+num = 1_000_000
+# soma
+print(1 + 1)
+num += 1
+# subtração
+print(5 - 3)
+num -= 1
+# multiplicação
+print(7 * 5)
+num *= 2
+# divisão
+print(8 / 2)
+num /= 2
+# divisão inteira
+print(10 // 6)
+num //= 2
+# elevação
+print(5 ** 3)
+num **= 2
+# resto da divisão
+print(5 % 2)
