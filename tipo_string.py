6c6144469cab6d73bdfae0ee9180b475ba9e31d6
Marcos Daniel <marcos.daniel0616@gmail.com>
2021-03-23 13:31:58 -0300

    Aula de strings finalizada!

--- tipo_string.py
+++ tipo_string.py
@@ -27,11 +27,39 @@ print(nome.upper())
 
 print(nome.lower())
 
-print(nome.split()) # Transforma em uma lista de strings
+print(nome.split()) # Transforma em uma lista de strings,
+
+
+print(nome[0:4]) # Slice de string
+print(nome[5:15]) # Slice de string
+
+print(nome.split()[0])
+print(nome.split()[1])
 """
 # - estiver entre aspas duplas triplas -> """234""", """a""", """True""", """42.3"""
 
 # [ 0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,   11,  12,  13, 14]
 # ['G', 'E', 'E', 'K', ' ', 'U', 'N', 'I', 'V', 'E', 'R', 'S', 'I', 'T', 'Y']
 nome = 'Geek University'
-print(nome.upper())
+
+"""
+[::-1] -> comece no primeiro elemento, vá até o último e inverta
+"""
+print(nome[::-1])  # Inversão da string
+
+print(nome.replace('G', 'P'))
+
+print(type(nome))
+
+texto = 'Socorram me subi no onibus em marrocos' # Palíndromo
+
+print(texto)
+
+print(texto[::-1])
+
+nome = 'ana' # Palíndromo
+
+print(nome)
+
+print(nome[::-1])
+
