61419d7adfe4e691b498f9cad84486a350d7a872
Marcos Daniel <marcos.daniel0616@gmail.com>
2021-04-01 00:03:51 -0300

    Aula de listas 1:51:19

--- listas.py
+++ listas.py
@@ -16,6 +16,16 @@ Já em Python:
 
 As listas em pythons são representadas por colchetes: []
 
+lista1 = [1, 99, 4, 27, 15, 22, 3, 1, 44, 42, 27]
+
+lista2 = ['G', 'e', 'e', 'k', ' ', 'U', 'n', 'i', 'v', 'e', 'r', 's', 'i', 't', 'y']
+
+lista3 = []
+
+lista4 = list(range(11))
+
+lista5 = list('Geek University')
+
 # Podemos facilmente checar se determinado valor está contido na lista
 num = 13
 if num in lista4:
@@ -125,17 +135,7 @@ curso = 'Programação,em,Python:,Essencial'
 print(curso)
 curso = curso.split(',')
 print(curso)
-"""
 
-lista1 = [1, 99, 4, 27, 15, 22, 3, 1, 44, 42, 27]
-
-lista2 = ['G', 'e', 'e', 'k', ' ', 'U', 'n', 'i', 'v', 'e', 'r', 's', 'i', 't', 'y']
-
-lista3 = []
-
-lista4 = list(range(11))
-
-lista5 = list('Geek University')
 
 # Convertendo uma lista em uma string
 lista6 = ['Programação', 'em', 'Python:', 'Essencial']
@@ -144,6 +144,120 @@ print(lista6)
 # Abaixo estamos falando: pega a lista 6, coloca espaço entre cada elemento e transforma em uma string
 curso = ' '.join(lista6)
 print(curso)
-
+# Abaixo estamos falando: pega a lista 6, coloca cifrão entre cada elemento e transforma em uma string
 curso = '$'.join(lista6)
 print(curso)
+
+# Podemos realmente colocar qualquer tipo de dado em uma lista, inclusive misturando esses dados
+
+lista6 = [1, 2.34, True, 'Geek', 'd', [1,2,3], 458394858]
+print(lista6)
+print(type(lista6))
+
+# Iterando sobre listas
+
+
+# Exemplo 1 - Utilizando for
+soma = 0
+for elemento in lista4:
+    print(elemento)
+    soma += elemento
+print(soma)
+
+# Exemplo 2 - Utilizando while
+
+carrinho = []
+produto = ''
+
+while produto != 'sair':
+    print("Adicione um produto na lista ou digite 'sair' para sair: ")
+    produto = input()
+    if produto != 'sair':
+        carrinho.append(produto)
+
+for produto in carrinho:
+    print(produto)
+
+# utilizando variáveis em listas
+numeros = [1, 2, 3, 4, 5]
+print(numeros)
+
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+num5 = 5
+
+numeros = [num1, num2, num3, num4, num5]
+print(numeros)
+
+# Fazemos acesso aos elementos de forma indexada
+
+#           0         1         2        3
+cores = ['Verde', 'Amarelo', 'Azul', 'Branco']
+print(cores[0])  # verde
+print(cores[1])  # amarelo
+print(cores[2])  # azul
+print(cores[3])  # branco
+
+# fazer acesso aos elementos de forma indexada reversa
+
+# para entender melhor o índice negativo, pense no índice como um círculo, onde
+# o final de um elemento está ligado ao início da lista
+print(cores[-1])  # branco
+print(cores[-2])  # azul
+print(cores[-3])  # amarelo
+print(cores[-4])  # Verde
+# print(cores[-5]) # erro, pois não existe o índice -5
+
+for cor in cores:
+    print(cor)
+
+indice = 0
+while indice < len(cores):
+    print(cores[indice])
+    indice += 1
+
+# Gerar indice em um for
+for indice, cor in enumerate(cores):
+    print(f'{indice} : {cor}')
+
+# listas aceitam valores repetidos
+lista = []
+lista.append(42)
+lista.append(42)
+lista.append(33)
+lista.append(33)
+lista.append(42)
+print(lista)
+
+
+"""
+
+# Outros métodos não tão importantes mas também úteis
+
+# Encontrar o índice de um elemento na lista
+
+numeros = [5, 6, 7, 5, 8, 9, 10]
+
+# Em qual índice está o valor 6?
+print(numeros.index(6))
+
+# Em qual índice da lista está o valor 9?
+print(numeros.index(9))
+
+# print(numeros.index(19)) # Gera valueError
+# OBS: caso não tenha este elemento na lista, será apresentado erro valueError
+
+# OBS: Retorna o índice do primeiro elemento encontrado
+print(numeros.index(5))
+
+# Podemos fazer busca dentro de um range, ou seja, qual índice começar a buscar
+print(numeros.index(5, 1)) # buscando a partir do índice 1
+print(numeros.index(5, 2)) # buscando a partir do índice 2
+
+# OBS: caso não tenha este elemento na lista, será apresentado erro valueError
+# print(numeros.index(5, 4)) # buscando a partir do índice 4
+
+# Podemos fazer busca dentro de um range, inicio/fim
+print(numeros.index(8, 3, 6)) # Buscar o índice do valor 8, entre 3 e 6
