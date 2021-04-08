8d8a8f7c3c0e25f95b363a20bd39d75e79df424f
Marcos Daniel <78885142+viniccastro1@users.noreply.github.com>
2021-04-08 00:21:17 -0300

    Aula de mapas finalizada!

---
+++ mapas.py
@@ -0,0 +1,46 @@
+"""
+Mapas -> Conhecidos em Python como dicionários
+
+Dicionários em Python são representados por chaves {}
+
+# Interar sobre dicionários
+for chave in receita:
+    print(chave)
+
+for chave in receita:
+    print(receita[chave])
+
+for chave in receita:
+    print(f'Em {chave} recebi R${receita[chave]}')
+
+# Acessando as chaves
+print(receita.keys())
+
+for chave in receita.keys():
+    print(receita[chave])
+
+# Acessando os valores
+print(receita.values())
+
+for valor in receita.values():
+    print(valor)
+
+print(receita)
+
+# Desempacotamento de dicionários
+
+print(receita.items())
+
+for chave, valor in receita.items():
+    print(f'chave={chave} valor={valor}')
+"""
+receita = {'jan': 100, 'fev': 250, 'mar': 400}
+
+# Soma*, Valor máximo*, Valor mínimo*, Tamanho
+
+# * se os valores forem todos inteiros ou reais.
+
+print(sum(receita.values()))
+print(max(receita.values()))
+print(min(receita.values()))
+print(len(receita))
