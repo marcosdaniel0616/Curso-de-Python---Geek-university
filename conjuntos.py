4fdef9e9b0758b712fc18fc1bd33321ecec7de3f
Marcos Daniel <78885142+viniccastro1@users.noreply.github.com>
2021-04-21 20:20:39 -0300

    Aula de conjuntos finalizada!! (finalmente)

--- conjuntos.py
+++ conjuntos.py
@@ -61,8 +61,6 @@ print(f'Dicionário: {dicionario} com {len(dicionario)} elementos')
 conjunto = {99, 2, 34, 23, 2, 12, 1, 44, 5, 34}
 print(f'Conjunto: {conjunto} com {len(conjunto)} elementos')
 
-"""
-
 # Assim como todo outro conjunto Python podemos colocar tipos de dados misturados em sets
 
 s = {1, 'B', True, 34, 22, 44}
@@ -72,3 +70,129 @@ print(type(s))
 # Podemos iterar em um Set normalmente
 for valor in s:
     print(valor)
+
+# Usos interessantes com sets
+
+# Imagine que fizemos um formulário de cadastro de visitantes em uma feira ou museu e os visitantes
+# informam manualmente a cidade de onde vieram.
+
+# Nós adicionamos cada cidade em uma lista Python, já que em uma lista podemos adicionar novos elementos e ter
+# Repetição.
+
+cidades = ['Belo Horizonte', 'São Paulo', 'Campo Grande', 'Cuiabá', 'Campo Grande', 'São Paulo', 'Cuiabá']
+
+print(cidades)
+print(len(cidades))
+
+# Agora precisamos saber quantas cidades distintas, ou seja, únicas, temos.
+
+# O que você faria? Faria um loop na lista....?
+
+# Podemos utilizar o set para isso:
+
+print(len(set(cidades)))
+
+# Adicionando elementos em um conjunto
+s = {1, 2, 3}
+
+s.add(4)
+s.add(4)  # Duplicidade não gera erro, simplesmente é ignorado e não é adicionado.
+print(s)
+
+# Adicionando elementos em um conjunto
+s = {1, 2, 3}
+
+# Forma 1
+s.remove(3)  # Não é índice! Informamos valor a ser removido.
+
+print(s)
+
+# OBS: Caso o valor não seja encontrado, será gerado o erro KeyError. Nenhum valor é retornado.
+
+# Forma 2
+s.discard(2)
+
+print(s)
+
+# OBS: Se o valor não for encontrado, nenhum erro é gerado.
+
+# Copiando um conjunto para outro...
+
+# Forma 1 - Deep Copy
+
+novo = s.copy()
+print(novo)
+
+novo.add(4)
+
+print(novo)
+print(s)
+
+# Forma 2 - shallow copy
+
+novo = s
+
+novo.add(4)
+
+print(novo)
+print(s)
+
+# Podemos remover todos os itens de um conjunto
+s.clear()
+print(s)
+
+# Métodos matemáticos de conjuntos
+
+# Imagine que temos 2 conjuntos: Um contendo estudantes do curso de Python e um
+# Contendo estudantes do curso de Java.
+
+estudantes_python = {'Marcos', 'Patricia', 'Ellen', 'Pedro', 'Julia', 'Guilherme'}
+estudantes_java = {'Fernando', 'Gustavo', 'Julia', 'Ana', 'Patricia'}
+
+# Veja que alguns alunos que estudam Python, também estudam Java.
+
+# Precisamos gerar um conjunto com nomes de estudantes únicos.
+
+# Forma 1 - Utilizando Union
+
+unicos1 = estudantes_python.union(estudantes_java)
+print(unicos1)
+# {'Marcos', 'Ellen', 'Julia', 'Fernando', 'Patricia', 'Ana', 'Gustavo', 'Pedro', 'Guilherme'}
+
+# Forma 2 - Utilizando o caractere pipe |
+unicos2 = estudantes_python | estudantes_java
+
+print(unicos2)
+
+# Gerar um conjunto de estudantes que estão em ambos os cursos
+
+# Forma 1 - Utilizando intersection
+
+ambos1 = estudantes_python.intersection(estudantes_java)
+print(ambos1)
+
+# Forma 2 - Utilizando o &
+
+ambos2 = estudantes_python & estudantes_java
+print(ambos2)
+
+# Gerar um conjunto de estudantes que não estão no outro curso
+
+so_python = estudantes_python.difference(estudantes_java)
+print(so_python)
+
+so_java = estudantes_java.difference(estudantes_python)
+print(so_java)
+"""
+
+# soma*, valor maximo*, valor mínimo*, tamanho
+
+# * se os valores forem todos inteiros ou reais
+
+s = {1, 2, 3, 4, 5, 6}
+
+print(sum(s))
+print(max(s))
+print(min(s))
+print(len(s))
+
