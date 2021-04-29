b2f486f7f895c91f58aa7a9f52dcbc40db8109d0
Marcos Daniel <marcos.daniel0616@gmail.com>
2021-04-28 22:28:30 -0300

    Aula de Deque finalizada!!

---
+++ deque.py
@@ -0,0 +1,37 @@
+"""
+Módulo Collections - Deque
+
+https://docs.python.org/3/library/collections.html#collections.deque
+
+Podemos dizer que o deque é uma lista de alta performance.
+
+
+"""
+
+# Importando
+from collections import deque
+
+# criando deques
+deq = deque('Geek')
+
+print(deq)
+
+# Adicionando elementos no deque
+
+deq.append('y')  # Adiciona no final
+
+print(deq)
+
+deq.appendleft('k')  # Adiciona no começo
+
+print(deq)
+
+# Remover elementos
+
+print(deq.pop())  # Remove e retorna o último elemento
+
+print(deq)
+
+print(deq.popleft())  # Remove e retorna o primeiro elemento
+
+print(deq)
