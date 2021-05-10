454370a1f262d18cd4cd6b82ac88b7532ca78b35
viniccastro1 <viniccastro@outlook.com>
2021-05-10 12:40:47 -0300

    Revert "Pull req test"
    This reverts commit e47946a1a2910d118b78849b530090726c74637e.

--- pullreqtest.py
+++
@@ -1,6 +0,0 @@
-"Daniel, isso é um comentário! (eu acho)"
-
-print("Isso é um comando!")
-print("E estou fazendo um fork do seu projeto...")
-print("Para nessa branch aqui...")
-print("Você aceitar meu pull request!")
