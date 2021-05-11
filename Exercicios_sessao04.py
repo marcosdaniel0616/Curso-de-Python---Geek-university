6a516492f0d2c7257e719340263b4214ef8fd7fe
Marcos Daniel <marcos.daniel0616@gmail.com>
2021-05-10 22:26:43 -0300

    Comecei a resolver os exerícios da sessão 4!

---
+++ Exercicios_sessao04.py
@@ -0,0 +1,31 @@
+"""
+# Ex 1: Faça um programa que leia um número inteiro e o imprima.
+numero_inteiro = int(input('Digite um número inteiro: '))
+print(numero_inteiro)
+
+# Ex 2: Faça um programa que leia um número real e o imprima.
+numero_real = float(input('Digite um número real: '))
+print(numero_real)
+
+# Ex 3: Peça ao usuário para digitar três valores inteiros e imprima a soma deles.
+primeiro = int(input('Digite o primeiro número: '))
+segundo = int(input('Digite o segundo número: '))
+terceiro = int(input('Digite o terceiro número: '))
+print(f'A soma dos números deu {primeiro + segundo + terceiro}')
+
+# Ex 4: Leia um número real e imprima o resultado do quadrado desse número.
+numero_real = float(input('Digite um número real: '))
+print(f'O quadrado de {numero_real} é {numero_real ** 2}')
+
+# Ex 5: Leia um número real e imprima a quinta parte desse número.
+numero_real = float(input('Digite um número real: '))
+print(f'A quinta parte de {numero_real} é {numero_real / 5}')
+
+# Ex 6: Leia uma temperatura em graus celsius e apresente-a convertida em graus Fahrenheit.
+# Fórmula: F = C * ( 9.0 / 5.0) + 32.0
+
+celsius = float(input('Digite a temperatura em graus: '))
+fahrenheit = celsius * (9 / 5) + 32
+print(f'{celsius} graus celsius convertidos para fahrenheit ficam {fahrenheit} graus fahrenheit')
+"""
+
